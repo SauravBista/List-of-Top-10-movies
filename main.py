2616d4c650867f5964e1043eb2503626b0f6c864
Saurav Bista <sauravbista10@gmail.com>
2024-08-25 18:21:51 +0545

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 db = SQLAlchemy(app)
 Bootstrap5(app)
 
-MOVIE_DB_API_KEY = "386c1a3dd9acc3af765cb44446412420"
+#login to movie data base to get free api
+MOVIE_DB_API_KEY = "your api here"
 MOVIE_DB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
 MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
 MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
